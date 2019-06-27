033fc9bdb968144e209c82e65dcc4a7b7f08044f
William Decker <willdecker@gmail.com>
2019-06-26 23:51:50 -0400

    Setup App functionality and UI styling

---
+++ timer.js
@@ -0,0 +1,82 @@
+// function setup() {
+//   createCanvas(400, 400);
+//   angleMode(DEGREES);
+// }
+
+// function draw() {
+//   clear();
+
+//   translate(200, 200);
+//   rotate(-90);
+
+//   let sc = second();
+
+//   strokeWeight(35);
+//   stroke(255, 100, 150);
+//   noFill();
+//   let secondAngle = map(sc, 0, 20, 0, 360);
+//   arc(0, 0, 300, 300, 0, secondAngle);
+// }
+
+let countdown;
+let selectedTimer;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const selectButtons = document.querySelectorAll('[data-time]');
+const startButton = document.querySelector('.timer__start');
+const stopButton = document.querySelector('.timer__stop');
+
+selectButtons.forEach(button => button.addEventListener('click', setTimer));
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+
+function timer(seconds) {
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // Check if timer hit 0
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = `(${display}) Focus Timer`;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+
+  endTime.textContent = `Ends At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function setTimer() {
+  clearInterval(countdown);
+  console.log(this.dataset);
+  selectedTimer = parseInt(this.dataset.time);
+  displayTimeLeft(selectedTimer);
+}
+
+function startTimer() {
+  timer(selectedTimer);
+}
+
+function stopTimer() {
+  clearInterval(countdown);
+}
